6b8b01dc2f60082b4fc03b5ded5f0c096d5ae499
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-28 16:21:52 +0300

    Create pch.h

---
+++ src/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+// Pre-compiled header for StealAndCook BakkesMod plugin
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+#include <algorithm>
+#include <random>
+#include <functional>
+#include <sstream>
+#include <iomanip>
+
+// BakkesMod SDK (expected at ../BakkesModSDK relative to repo)
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/pluginwindow.h"
+#include "bakkesmod/wrappers/GameWrapper.h"
+#include "bakkesmod/wrappers/CarWrapper.h"
+#include "bakkesmod/wrappers/GameObject/CarComponent/BoostWrapper.h"
+#include "imgui/imgui.h"
